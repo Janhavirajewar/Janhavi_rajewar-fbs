a0f2bf0bf03c2794a84385543930a6ded2807dcb
Janhavi Rajewar <janhavirajewar@gmail.com>
2025-06-06 12:30:15 +0530

    Update Assignment2

--- c programming/Assignments/Assignment2/menudriven.c
+++ c programming/Assignments/Assignment2/menudriven.c
@@ -2,28 +2,29 @@
 void main()
 {
 	int n1,n2,c;
-	int ch;
+	char ch;
 	printf("Enter two numbers");
 	scanf("%d%d",&n1,&n2);
-	printf("enter your choise ");
-	printf("1. for addition,2. for sub,3. for mul,4. for div");
-	scanf("%d",&ch);
-	if(ch==1)
+	printf("'+'. for addition,'-'. for sub,'*'. for mul,'/'. for div");
+	printf("\n enter your choice ");
+	fflush(stdin);
+	scanf("%c",&ch);
+	if(ch=='+')
 	{
 		c=n1+n2;
 		printf("\n Addition of two nos is %d",c);
 	}
-	else if(ch==2)
+	else if(ch=='-')
 	{
 		c=n1-n2;
 		printf("\n substration of two nos is %d",c);
 	}
-	else if(ch==3)
+	else if(ch=='*')
 	{
 		c=n1*n2;
 		printf("\n multiplication of two nos is %d",c);
 	}
-	else if(ch==4)
+	else if(ch=='/')
 	{
 		c=n1/n2;
 		printf("\n divition of two nos is %d",c);
