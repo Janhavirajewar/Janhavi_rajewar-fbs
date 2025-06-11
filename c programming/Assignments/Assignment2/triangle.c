38cffd9b1e3b8de4f0542badb80f3ea46affaad8
Janhavi Rajewar <janhavirajewar@gmail.com>
2025-06-11 20:40:06 +0530

    update Assignment2

--- c programming/Assignments/Assignment2/triangle.c
+++ c programming/Assignments/Assignment2/triangle.c
@@ -3,7 +3,7 @@ void main()
 {
 	float s1,s2,s3;
 	printf("Enter three sides of triangle");
-	scanf("%f %f %f",&s2,&s2,&s3);
+	scanf("%f %f %f",&s1,&s2,&s3);
 	if(s1==s2&&s2==s3)
 	{
 		printf("Triangle is equilateral");
